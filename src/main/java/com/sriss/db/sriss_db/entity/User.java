e67dc9745c01fbba5a18e21c1d54786586da0924
srijansingh1325srijan@gmail.com <srijansingh1325srijan@gmail.com>
2020-06-18 10:50:38 +0000

    File generated by Skaffolder - user: srijansingh1325srijan@gmail.com

---
+++ src/main/java/com/sriss/db/sriss_db/entity/User.java
@@ -0,0 +1,10 @@
+package com.sriss.db.sriss_db.entity;
+
+import com.sriss.db.sriss_db.entity.base.UserBase;
+
+public class User extends UserBase {
+
+	//OVERRIDE HERE YOUR CUSTOM MAPPER
+	
+	
+}
---
+++ src/main/java/com/sriss/db/sriss_db/service/UserService.java
@@ -0,0 +1,10 @@
+package com.sriss.db.sriss_db.service;
+
+import com.sriss.db.sriss_db.service.base.UserBaseService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService extends UserBaseService {
+
+	//OVERRIDE HERE YOUR CUSTOM SERVICE
+}
